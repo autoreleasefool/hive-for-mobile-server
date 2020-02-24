ad6a13f81162dc7e5324a712c6d28c0a3de7e27b
Joseph Roque <contact@josephroque.ca>
2020-02-24 09:04:35 +0100

    Bump HiveEngine to 2.4.1

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.4"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.3.1"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        .package(url: "https://github.com/josephroquedev/hive-engine.git", from: "2.4.0"),
+        .package(url: "https://github.com/josephroquedev/hive-engine.git", from: "2.4.1"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Authentication", "FluentSQLite", "Vapor", "HiveEngine"]),

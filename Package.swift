955777eb139afe5f2880f9579de19906201e506c
Joseph Roque <contact@josephroque.ca>
2020-04-16 10:01:03 +0200

    Update to Swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
@@ -10,13 +10,21 @@ let package = Package(
 		.library(name: "Hive-for-iOS-Server", targets: ["App"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/vapor/auth.git", .exact("2.0.4")),
-		.package(url: "https://github.com/vapor/vapor.git", .exact("3.3.1")),
-		.package(url: "https://github.com/vapor/fluent-sqlite.git", .exact("3.0.0")),
-		.package(url: "https://github.com/josephroquedev/hive-engine.git", .exact("2.4.2")),
+		.package(name: "Auth", url: "https://github.com/vapor/auth.git", from: "2.0.4"),
+		.package(name: "Vapor", url: "https://github.com/vapor/vapor.git", from: "3.3.1"),
+		.package(name: "FluentSQLite", url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+		.package(name: "HiveEngine", url: "https://github.com/josephroquedev/hive-engine.git", from: "2.5.1"),
 	],
 	targets: [
-		.target(name: "App", dependencies: ["Authentication", "FluentSQLite", "Vapor", "HiveEngine"]),
+		.target(
+			name: "App",
+			dependencies: [
+				.product(name: "Authentication", package: "Auth"),
+				.product(name: "FluentSQLite", package: "FluentSQLite"),
+				.product(name: "Vapor", package: "Vapor"),
+				.product(name: "HiveEngine", package: "HiveEngine"),
+			]
+		),
 		.target(name: "Run", dependencies: ["App"]),
 		.testTarget(name: "AppTests", dependencies: ["App"]),
 	]

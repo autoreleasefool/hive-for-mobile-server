31cc62687bfa9cd9e4bf7cd8aed3caf9e20d7e00
Joseph Roque <contact@josephroque.ca>
2020-06-13 09:46:12 -0400

    Save db to file

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -32,7 +32,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 	services.register(middlewares)
 
 	// Configure a SQLite database
-	let sqlite = try SQLiteDatabase(storage: .memory)
+	let sqlite = try SQLiteDatabase(storage: .file(path: "db.sqlite"))
 
 	// Register the configured SQLite database to the database config.
 	var databases = DatabasesConfig()

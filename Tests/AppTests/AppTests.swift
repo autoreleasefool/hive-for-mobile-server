dd2f4c8d978aad985a384cdc3ef1df53488e931d
Joseph Roque <contact@josephroque.ca>
2020-06-20 18:27:01 -0400

    Basic route test

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,15 +1,23 @@
+//
+//  AppTests.swift
+//  Hive-for-Mobile-Server
+//
+//  Created by Joseph Roque on 2020-06-20.
+//  Copyright © 2020 Joseph Roque. All rights reserved.
+//
+
 @testable import App
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testHelloWorld() throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
+	func testItWorks() throws {
+		let app = Application(.testing)
+		defer { app.shutdown() }
+		try configure(app)
 
-        try app.test(.GET, "hello") { res in
-            XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
-        }
-    }
+		try app.test(.GET, "/") { res in
+			XCTAssertEqual(res.status, .ok)
+			XCTAssertEqual(res.body.string, "It works!")
+		}
+	}
 }

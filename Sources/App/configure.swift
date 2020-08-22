3077ee075a79906556a1701617a59d59a510d82a
Joseph Roque <github@josephroque.ca>
2020-08-21 23:20:30 -0400

    Support PORT override from environment

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,6 +15,10 @@ public func configure(_ app: Application) throws {
 	// uncomment to serve files from /Public folder
 	// app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+	if let portEnv = Environment.get("PORT"), let port = Int(portEnv) {
+		app.http.server.configuration.port = port
+	}
+
 	app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
 	app.migrations.add(CreateUser())
